b069722c1f117e21fb048afcdba38aad49c009f4
João Pedro Basso Audibert <hotsjoao@gmail.com>
2022-01-08 21:34:25 -0300

    :sparkles: Test suite for Movie complete

--- TakeYourTicket/Tests/MovieTests.cs
+++ TakeYourTicket/Tests/MovieTests.cs
@@ -11,8 +11,11 @@ namespace Tests
     {
         private Movie _movieMock = new Movie("Carros", 128, "Relâmpago McQueen");
         private Movie _movieMock2 = new Movie("Carros 2", 116, "Relâmpago McQueen corre no circuito europeu");
+        private Movie _emptyTitleMock = new Movie("", 116, "Relâmpago McQueen corre no circuito europeu");
+        private Movie _emptyDurationMock = new Movie("Carros 3", 0, "Relâmpago McQueen corre no circuito europeu");
+        private Movie _emptySynopsisMock = new Movie("Carros 3", 116, "");
 
-        private IMovieRepository _movieRepository = new MovieRepository();
+        private IMovieRepository _movieRepository = new EFMovieRepository();
 
         [TestMethod]
         public async Task TestMethodCreateMovie()
@@ -25,18 +28,45 @@ namespace Tests
             Assert.AreEqual("Relâmpago McQueen", createdMovie.Synopsis);
         }
 
+        public async Task TestMethodUpdateMovie()
+        {
+            await _movieRepository.Create(_movieMock);
+            Movie updatedMovie = await _movieRepository.Update(_movieMock2);
+
+            Assert.IsNotNull(updatedMovie);
+            Assert.AreNotEqual(_movieMock, updatedMovie);
+        }
+
         [TestMethod]
         public async Task TestMethodTitleMustBeUnique()
         {
             Movie createdMovie = await _movieRepository.Create(_movieMock);
-            Movie createdMovie2 = await _movieRepository.Create(_movieMock);
 
-            Assert.IsInstanceOfType("", System.Exception);
+            Assert.ThrowsException<System.ArgumentException>(async () => await _movieRepository.Create(_movieMock));
+        }
 
-            Assert.IsNotNull(createdMovie);
-            Assert.AreEqual("Carros", createdMovie.Title);
-            Assert.AreEqual(128, createdMovie.Duration);
-            Assert.AreEqual("Relâmpago McQueen", createdMovie.Synopsis);
+        [TestMethod]
+        public void TestMethodEmptyTitle()
+        {
+            Assert.ThrowsException<System.ArgumentNullException>(async () => await _movieRepository.Create(_emptyTitleMock));
+        }
+
+        [TestMethod]
+        public void TestMethodDurationEqualsZero()
+        {
+            Assert.ThrowsException<System.ArgumentException>(async () => await _movieRepository.Create(_emptyDurationMock));
+        }
+
+        [TestMethod]
+        public void TestMethodEmptySynopsis()
+        {
+            Assert.ThrowsException<System.ArgumentNullException>(async () => await _movieRepository.Create(_emptySynopsisMock));
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            // implementar a limpeza da tabela do banco
         }
     }
 }
